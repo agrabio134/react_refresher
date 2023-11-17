78c464632ad196117c5db586e7e1e83291052b06
harvey <agrabioharvey@gmail.com>
2023-11-17 16:43:31 +0800

    create appointment form

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./Style/MainLayout.css";
-import HomePage from "../pages/HomePage";
+import HomePage from "../pages/Home/HomePage";
 // import AboutPage from "../pages/AboutUs";
 import ServicePage from "../pages/ServicePage";
 import ProfilePage from "../pages/Profile/ProfilePage";
 import VerifyEmail from "../Auth/VerifyEmail";
 import EmailVerifiedComponent from "../Auth/SuccessMsg";
 import AlreadyVerifiedComponent from "../Auth/ErrorVerification";
-import BlogPage from "../pages/BlogPage";
+import BlogPage from "../pages/blog/BlogPage";
+import AppointmentForm from "../pages/appointment/AppointmentForm";
 
 const MainLayout = () => {
   return (
@@ -20,6 +21,9 @@ const MainLayout = () => {
             <div className="main_nav">
               <Link to="/" className="nav_list">
                 Home
+              </Link>
+              <Link to="/appointments" className="nav_list">
+                Appointment
               </Link>
               <Link to="service" className="nav_list">
                 Service
@@ -36,6 +40,7 @@ const MainLayout = () => {
             <div className="main_page">
               <Routes>
                 <Route path="/" element={<HomePage />} />
+                <Route path="/appointments" element={<AppointmentForm />} />
                 <Route path="/service" element={<ServicePage />} />
                 <Route path="/blogs" element={<BlogPage />} />
                 <Route path="/profile" element={<ProfilePage/>} />
---
+++ src/components/pages/appointment/AppointmentForm.jsx
@@ -0,0 +1,240 @@
+import React, { useState, useEffect } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import Swal from "sweetalert2";
+
+const AppointmentForm = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [timeSlots, setTimeSlots] = useState([]);
+  const [selectedTimeSlot, setSelectedTimeSlot] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [hasPendingAppointment, setHasPendingAppointment] = useState(false);
+  const [bookedTimeSlots, setBookedTimeSlots] = useState([]);
+
+  useEffect(() => {
+    fetchTimeSlots(selectedDate);
+    checkPendingAppointment(selectedDate);
+  }, [selectedDate]);
+
+  const fetchTimeSlots = async (date) => {
+    try {
+      const formattedDate = date.toISOString().split("T")[0];
+  
+      const response = await fetch(
+        `http://localhost/api/get_available_time_slots/${formattedDate}`
+      );
+      const rawResponse = await response.text();
+      const timeSlotsStartIndex = rawResponse.indexOf("[");
+      const timeSlotsEndIndex = rawResponse.lastIndexOf("]");
+      const timeSlotsJSON = rawResponse.substring(
+        timeSlotsStartIndex,
+        timeSlotsEndIndex + 1
+      );
+      const parsedTimeSlots = JSON.parse(timeSlotsJSON);
+  
+      const bookedResponse = await fetch(
+        `http://localhost/api/get_booked_time_slots/${formattedDate}`
+      );
+      const bookedTimeSlotsData = await bookedResponse.json();
+      const bookedTimeSlotsArray = Array.isArray(bookedTimeSlotsData)
+        ? bookedTimeSlotsData
+        : [];
+  
+      setTimeSlots(parsedTimeSlots);
+      setBookedTimeSlots(bookedTimeSlotsArray);
+    } catch (error) {
+      console.error("Error fetching time slots:", error);
+    }
+  };
+
+  const checkPendingAppointment = async (date) => {
+    try {
+      const formattedDate = date.toISOString().split("T")[0];
+      const response = await fetch(
+        `http://localhost/api/check_pending_appointment/${formattedDate}`
+      );
+      const data = await response.json();
+      setHasPendingAppointment(data.hasPendingAppointment);
+    } catch (error) {
+      console.error("Error checking pending appointment:", error);
+    }
+  };
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+    fetchTimeSlots(date);
+    checkPendingAppointment(date);
+  };
+
+  const handleTimeSlotChange = (time) => {
+    setSelectedTimeSlot(time);
+  };
+
+  const handleAppointmentSubmit = async () => {
+    const authToken = localStorage.getItem("authToken");
+    if (!authToken) {
+      Swal.fire({
+        title: "Error",
+        icon: "error",
+        text: "You must be logged in to make an appointment.",
+        confirmButtonText: "Okay",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          window.location.href = "/profile";
+        }
+      });
+      return;
+    }
+
+    let userId;
+
+    try {
+      const response = await fetch(
+        `http://localhost/api/get_user_id/${authToken}`,
+        {
+          method: "GET",
+        }
+      );
+
+      const responseData = await response.text();
+      const jsonObjects = responseData.split("}{").map((obj, index, array) => {
+        return index < array.length - 1 ? `${obj}}` : obj;
+      });
+
+      for (let i = 0; i < jsonObjects.length; i++) {
+        const jsonObject = jsonObjects[i];
+
+        try {
+          const data = JSON.parse(jsonObject);
+
+          if (
+            data.status &&
+            data.status.remarks === "success" &&
+            data.payload &&
+            typeof data.payload[0] !== "undefined" &&
+            typeof data.payload[0].id !== "undefined"
+          ) {
+            userId = data.payload[0].id;
+            break;
+          }
+        } catch (error) {
+          console.error("Error parsing JSON:", error);
+        }
+      }
+
+      if (!userId) {
+        console.error(
+          "Error fetching user ID: No valid user ID found in the response"
+        );
+        Swal.fire({
+          icon: "error",
+          title: "Error",
+          text: "Failed to fetch user ID. No valid user ID found in the response.",
+        });
+      }
+    } catch (error) {
+      console.error("Error fetching user ID:", error);
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: "Failed to fetch user ID. Please try again.",
+      });
+    }
+
+    setIsSubmitting(true);
+
+    try {
+      if (hasPendingAppointment) {
+        Swal.fire({
+          icon: "error",
+          title: "Error",
+          text: "You already have a pending appointment. You can only have one appointment at a time.",
+        });
+        return;
+      }
+
+      if (bookedTimeSlots.includes(selectedTimeSlot)) {
+        Swal.fire({
+          icon: "error",
+          title: "Error",
+          text: "The selected time slot is already booked. Please choose another time.",
+        });
+        return;
+      }
+
+      await fetch("http://localhost/api/create_appointments", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${authToken}`,
+        },
+        body: JSON.stringify({
+          date: selectedDate.toISOString().split("T")[0],
+          time: selectedTimeSlot,
+          user_Id: userId,
+        }),
+      });
+
+      Swal.fire({
+        icon: "success",
+        title: "Success",
+        text: "Appointment created successfully.",
+      });
+    } catch (error) {
+      console.error("Error creating appointment:", error);
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: "Failed to create appointment. Please try again.",
+      });
+    } finally {
+      setIsSubmitting(false);
+      fetchTimeSlots(selectedDate);
+      checkPendingAppointment(selectedDate);
+    }
+  };
+
+  const formatTimeSlotLabel = (time) => {
+    const [hours, minutes, seconds] = time.split(":");
+    const formattedHours = parseInt(hours, 10) % 12 || 12;
+    const period = parseInt(hours, 10) < 12 ? "AM" : "PM";
+    return `${formattedHours}:${minutes} ${period}`;
+  };
+
+  return (
+    <div>
+      <label>Select Date:</label>
+      <Calendar
+        onChange={handleDateChange}
+        value={selectedDate}
+        className="static-calendar"
+        minDate={new Date()}
+      />
+
+      <label>Select Time Slot:</label>
+      <select
+        value={selectedTimeSlot}
+        onChange={(e) => handleTimeSlotChange(e.target.value)}
+        disabled={isSubmitting}
+      >
+        {timeSlots.map((time) => (
+          <option
+            key={time}
+            value={time}
+            disabled={bookedTimeSlots.includes(time) || isSubmitting}
+          >
+            {formatTimeSlotLabel(time)}
+          </option>
+        ))}
+      </select>
+
+      <button onClick={handleAppointmentSubmit} disabled={isSubmitting}>
+        {isSubmitting ? "Submitting..." : "Submit Appointment"}
+      </button>
+
+      {hasPendingAppointment && <p>You have a pending appointment.</p>}
+    </div>
+  );
+};
+
+export default AppointmentForm;
