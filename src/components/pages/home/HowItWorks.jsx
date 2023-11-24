1ecf1cb45ce0bbc00d730b0accdb024481eaf708
harvey <agrabioharvey@gmail.com>
2023-11-24 15:41:45 +0800

    fix slider

--- src/components/pages/home/HowItWorks.jsx
+++ src/components/pages/home/HowItWorks.jsx
@@ -1,42 +1,83 @@
-import "./Style/HIW.css";
-import "./js/slinder.js";
-
+import React, { useState, useEffect } from 'react';
+import './Style/HIW.css';
 
 const HowItWorksPage = () => {
+  const [slideIndex, setSlideIndex] = useState(1);
+
+  const plusSlides = (n) => {
+    showSlides(slideIndex + n);
+  };
+
+  const currentSlide = (n) => {
+    showSlides(n);
+  };
+
+  const showSlides = (n) => {
+    let i;
+    const slides = document.getElementsByClassName('mySlides');
+    const dots = document.getElementsByClassName('dot');
+
+    if (n > slides.length) {
+      setSlideIndex(1);
+    } else if (n < 1) {
+      setSlideIndex(slides.length);
+    } else {
+      setSlideIndex(n);
+    }
+
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = 'none';
+    }
+
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(' active', '');
+    }
+
+    slides[slideIndex - 1].style.display = 'block';
+    dots[slideIndex - 1].className += ' active';
+  };
+
+  useEffect(() => {
+    // Initial call to showSlides to hide all images
+    showSlides(slideIndex);
+  }, [slideIndex]);
+
   return (
     <>
       <section className="main_service_container">
-      <div class="slideshow-container">
+        <div className="slideshow-container">
+          <div className="mySlides fade">
+            <div className="numbertext">1 / 3</div>
+            <img src="src/assets/img/time.jpg" alt="" />
+            <div className="text">Caption Text</div>
+          </div>
 
-<div class="mySlides fade">
-  <div class="numbertext">1 / 3</div>
-  <img src="src/assets/img/time.jpg" alt="" />
-  <div class="text">Caption Text</div>
-</div>
+          <div className="mySlides fade">
+            <div className="numbertext">2 / 3</div>
+            <img src="src/assets/img/time.jpg" alt="" />
+            <div className="text">Caption Two</div>
+          </div>
 
-<div class="mySlides fade">
-  <div class="numbertext">2 / 3</div>
-  <img src="src/assets/img/time.jpg" alt="" />
-  <div class="text">Caption Two</div>
-</div>
+          <div className="mySlides fade">
+            <div className="numbertext">3 / 3</div>
+            <img src="src/assets/img/time.jpg" alt="" />
+            <div className="text">Caption Three</div>
+          </div>
 
-<div class="mySlides fade">
-  <div class="numbertext">3 / 3</div>
-  <img src="src/assets/img/time.jpg" alt="" />
-  <div class="text">Caption Three</div>
-</div>
+          <a className="prev" onClick={() => plusSlides(-1)}>
+            ❮
+          </a>
+          <a className="next" onClick={() => plusSlides(1)}>
+            ❯
+          </a>
+        </div>
+        <br />
 
-<a class="prev" onclick="plusSlides(-1)">❮</a>
-<a class="next" onclick="plusSlides(1)">❯</a>
-
-</div>
-<br />
-
-<div>
-  <span class="dot" onclick="currentSlide(1)"></span> 
-  <span class="dot" onclick="currentSlide(2)"></span> 
-  <span class="dot" onclick="currentSlide(3)"></span> 
-</div>
+        <div>
+          <span className="dot" onClick={() => currentSlide(1)}></span>
+          <span className="dot" onClick={() => currentSlide(2)}></span>
+          <span className="dot" onClick={() => currentSlide(3)}></span>
+        </div>
       </section>
     </>
   );
