2fc8e0a3217da5afca3c5267380328c37fe5f83f
JP <202011111@gordoncollege.edu.ph>
2023-11-26 02:32:45 +0800

    how it works page done

--- src/components/pages/home/HowItWorks.jsx
+++ src/components/pages/home/HowItWorks.jsx
@@ -48,7 +48,7 @@ const HowItWorksPage = () => {
       <div className='whole_hiw_container'>
         <div className='hiw_container'>
         <div className='hiw_content'>
-          <h2>How it works</h2>
+          <h2>How it works?</h2>
           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum, doloribus magni atque voluptatem architecto odit delectus ad perspiciatis id labore non qui cum natus eos amet, accusamus sed debitis! Iure.</p>
         </div>
 
@@ -57,19 +57,27 @@ const HowItWorksPage = () => {
           <div className="mySlides fade">
             <div className="numbertext">1 / 3</div>
             <img src="src/assets/img/HIW1.jpg" alt="" />
-            <div className="text">Caption Text</div>
+            <div className="text">
+              <h2>Register Your Pet</h2>
+              <p>Form Fields for Pet Name, Breed, Age, etc.</p>
+            </div>
           </div>
 
           <div className="mySlides fade">
             <div className="numbertext">2 / 3</div>
             <img src="src/assets/img/HIW2.jpg" alt="" />
-            <div className="text">Caption Two</div>
+            <div className="text"><h2>Select Service</h2>
+              <p>Dropdown Menu for Services: Vet Check-up, Grooming, Vaccinations, etc.</p>
+              </div>
           </div>
 
           <div className="mySlides fade">
             <div className="numbertext">3 / 3</div>
             <img src="src/assets/img/HIW3.jpg" alt="" />
-            <div className="text">Caption Three</div>
+            <div className="text">
+              <h2>Pick a Time</h2>
+              <p>Form Fields for Pet Name, Breed, Age, etc.</p>
+            </div>
           </div>
 
           <a className="prev" onClick={() => plusSlides(-1)}>
