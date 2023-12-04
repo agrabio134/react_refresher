d5c9e4272434b9589742e40aac4b1d4e22b0bae8
harvey <agrabioharvey@gmail.com>
2023-12-05 02:35:27 +0800

    add func to gallery

--- src/components/pages/gallery/GalleryPage.jsx
+++ src/components/pages/gallery/GalleryPage.jsx
@@ -1,49 +1,50 @@
-import React, { useState, useEffect } from 'react';
-import './GalleryPage.css'; // Import your CSS file
-
+import React, { useState, useEffect } from "react";
+import "./GalleryPage.css"; // Import your CSS file
+import { Modal, Button, Descriptions, Image, Breadcrumb, Spin } from "antd";
 
 const GalleryPage = () => {
   const [galleryData, setGalleryData] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [currentImage, setCurrentImage] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('http://localhost/api/get_gallery');
-        const textData = await response.text(); // Get the raw response as text
+        const response = await fetch("http://localhost/api/get_gallery");
+        const textData = await response.text();
 
-        // console.log('Raw Response:', textData); // Log the raw response content
+        const jsonObjects = textData.split("}{").map((json, index, array) => {
+          return index === 0
+            ? json + "}"
+            : index === array.length - 1
+            ? "{" + json
+            : "{" + json + "}";
+        });
 
-        // Split the response into individual JSON objects
-        const jsonObjects = textData
-          .split('}{')
-          .map((json, index, array) => {
-            // Add '}' back to the first object and '{' back to the last object
-            return index === 0
-              ? json + '}'
-              : index === array.length - 1
-              ? '{' + json
-              : '{' + json + '}';
-          });
-
-        // Parse each JSON object
         jsonObjects.forEach((json) => {
           try {
             const parsedResult = JSON.parse(json);
 
-            if (parsedResult.status && parsedResult.status.remarks === 'success') {
+            if (
+              parsedResult.status &&
+              parsedResult.status.remarks === "success"
+            ) {
               setGalleryData(parsedResult.payload);
             } else {
-              // console.error('Failed to fetch gallery data:', parsedResult.error);
+              console.error(
+                "Failed to fetch gallery data:",
+                parsedResult.error
+              );
             }
           } catch (jsonError) {
-            console.error('Error parsing JSON:', jsonError);
+            console.error("Error parsing JSON:", jsonError);
           }
         });
-        
+
         setLoading(false);
       } catch (error) {
-        console.error('Error fetching gallery data:', error);
+        console.error("Error fetching gallery data:", error);
         setLoading(false);
       }
     };
@@ -51,36 +52,123 @@ const GalleryPage = () => {
     fetchData();
   }, []);
 
+  const handleImageClick = (image) => {
+    setCurrentImage(image);
+    setModalVisible(true);
+  };
+
+  const handleModalClose = () => {
+    setModalVisible(false);
+  };
+
+  const handleNextImage = () => {
+    const currentIndex = galleryData.findIndex(
+      (item) => item.id === currentImage.id
+    );
+    const nextIndex = currentIndex + 1;
+
+    if (nextIndex >= galleryData.length) {
+      setCurrentImage(galleryData[0]);
+    } else {
+      setCurrentImage(galleryData[nextIndex]);
+    }
+  };
+
+  const handlePrevImage = () => {
+    const currentIndex = galleryData.findIndex(
+      (item) => item.id === currentImage.id
+    );
+    const prevIndex = currentIndex - 1;
+
+    if (prevIndex < 0) {
+      setCurrentImage(galleryData[galleryData.length - 1]);
+    }
+    else {
+      setCurrentImage(galleryData[prevIndex]);
+    }
+    
+  };
+
   return (
-    <section className='main-gallery-container'>
-    <div className='gallery-welcome-container'>
-    <div className="gallery-content">
-          <h1><span className="span">Our</span> Gallery</h1>
-          <ul className='gallery-breadcrumbs'>
-          <a href="">Home </a>
-          <li href="">/</li>
-          <a href="">Gallery</a>
+    <section className="main-gallery-container">
+      <div className="gallery-welcome-container">
+        <div className="gallery-content">
+          <h1>
+            <span className="span">Our</span> Gallery
+          </h1>
+          <ul className="gallery-breadcrumbs">
+            <a href="">Home </a>
+            <li href="">/</li>
+            <a href="">Gallery</a>
           </ul>
         </div>
-    </div>
-    <div className="gallery-container">
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <ul className="image-list">
-          {galleryData.map((item) => (
-            <li key={item.id} className="image-item">
-              <img src={`${item.image}`} alt={item.image} />
-              <div className="image-details">
-                <p>{item.description}</p>
-                <p>Upload Date: {item.upload_date}</p>
-              </div>
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
-</section>
+      </div>
+      <div className="gallery-container">
+        {loading ? (
+          <Spin tip="Loading...">
+            <p>Loading...</p>
+          </Spin>
+        ) : (
+          <ul className="image-list">
+            {galleryData.map((item) => (
+              <li
+                key={item.id}
+                className="image-item"
+                onClick={() => handleImageClick(item)}
+              >
+                <img
+                  src={`${item.image}`}
+                  alt={item.image}
+                  style={{
+                    cursor: "pointer",
+                  }}
+                />
+                <div className="image-details">
+                  <Descriptions>
+                    <Descriptions.Item label="Description">
+                      {item.description}
+                    </Descriptions.Item>
+
+                    {/* <Descriptions.Item label='Upload Date'>{item.upload_date}</Descriptions.Item> */}
+                  </Descriptions>
+                </div>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+      <Modal
+        visible={modalVisible}
+        onCancel={handleModalClose}
+        footer={[
+          <Button key="prev" onClick={handlePrevImage}>
+            Prev
+          </Button>,
+          
+          // <Button key="close" onClick={handleModalClose}>
+          //   Close
+          // </Button>,
+          <Button key="next" onClick={handleNextImage}>
+            Next
+          </Button>,
+
+        ]}
+      >
+        {currentImage && (
+          <div>
+            <Image src={`${currentImage.image}`} alt={currentImage.image} />
+            <div className="image-details">
+              <p>{currentImage.description}</p>
+              <Descriptions>
+                <Descriptions.Item label="Upload Date">
+                  {currentImage.upload_date}
+                </Descriptions.Item>
+              </Descriptions>
+            </div>
+          </div>
+        )}
+      </Modal>
+    </section>
   );
 };
 
