322cc52b811d4956df444f749d93bf9a2b05d46c
harvey <agrabioharvey@gmail.com>
2024-05-02 14:46:28 +0800

    fix forgot pass

--- src/components/Auth/pages/ForgotPassword.jsx
+++ src/components/Auth/pages/ForgotPassword.jsx
@@ -10,6 +10,10 @@ const ForgotPassword = () => {
     email: "",
   });
 
+  const [forgotPasswordClicked, setForgotPasswordClicked] = useState(false);
+  const [verifyCodeClicked, setVerifyCodeClicked] = useState(false);
+  const [resetPasswordClicked, setResetPasswordClicked] = useState(false);
+
   const handleChange = (event) => {
     const { name, value } = event.target;
     setFormData({
@@ -19,13 +23,21 @@ const ForgotPassword = () => {
   };
 
   const handleForgotPassword = async () => {
+    if (forgotPasswordClicked) return;
+
     try {
-      const response = await fetch("https://happypawsolongapo.com/api/send_forgot_password_code/" + formData.email, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
+      setForgotPasswordClicked(true);
+
+      const response = await fetch(
+        "https://happypawsolongapo.com/api/send_forgot_password_code/" +
+          formData.email,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
 
       if (response.ok) {
         // Proceed to the next step (verification)
@@ -57,20 +69,34 @@ const ForgotPassword = () => {
   };
 
   const handleVerifyCode = async () => {
-    try {
-      const response = await fetch("https://happypawsolongapo.com/api/verify_forgot_password_otp", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          email: formData.email, // Use formData.email instead of just email
-          entered_otp: verificationCode, // Rename to entered_otp to match backend parameter
-        }),
-      });
+    if (verifyCodeClicked) return;
 
-      if (response.ok) {
-        // Proceed to the next step (password reset)
+    try {
+      setVerifyCodeClicked(true);
+
+      const response = await fetch(
+        "https://happypawsolongapo.com/api/verify_forgot_password_otp/" +
+          formData.email +
+          "/" +
+          verificationCode,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      console.log(response);
+
+      if (response.status === 200) {
+        // Proceed to the next step (password reset).
+        Swal.fire({
+          title: "Code verified",
+          icon: "success",
+          confirmButtonText: "Okay",
+        });
+
         setStep(3);
       } else {
         // Handle error
@@ -81,6 +107,8 @@ const ForgotPassword = () => {
           text: "Invalid verification code",
           confirmButtonText: "Okay",
         });
+
+        setVerifyCodeClicked(false);
       }
     } catch (error) {
       console.error("Error occurred while verifying code:", error);
@@ -94,8 +122,58 @@ const ForgotPassword = () => {
   };
 
   const handlePasswordReset = async () => {
+
+    if (newPassword !== confirmPassword) {
+      Swal.fire({
+        title: "Error",
+        icon: "error",
+        text: "Passwords do not match",
+        confirmButtonText: "Okay",
+      });
+      return;
+    }
+    
+
+    if (resetPasswordClicked) return;
+
     try {
-      // Implement password reset logic
+      setResetPasswordClicked(true);
+
+      const response = await fetch(
+        "https://happypawsolongapo.com/api/reset_password/" +
+          formData.email +
+          "/" +
+          newPassword,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (response.ok) {
+        // Password reset successful
+        Swal.fire({
+          title: "Password reset successful",
+          icon: "success",
+          confirmButtonText: "Okay",
+        });
+
+        // Redirect to login page
+        window.location.href = "/auth/login";
+      } else {
+        // Handle error
+        console.error("Error resetting password");
+        Swal.fire({
+          title: "Error",
+          icon: "error",
+          text: "Failed to reset password",
+          confirmButtonText: "Okay",
+        });
+
+        setResetPasswordClicked(false);
+      }
     } catch (error) {
       console.error("Error occurred while resetting password:", error);
       Swal.fire({
@@ -107,6 +185,7 @@ const ForgotPassword = () => {
     }
   };
 
+
   return (
     <div className="forgot-password-container">
       {step === 1 && (
@@ -115,10 +194,11 @@ const ForgotPassword = () => {
             type="email"
             name="email"
             placeholder="Email Address"
+            required
             value={formData.email}
             onChange={handleChange}
           />
-          <button onClick={handleForgotPassword}>Forgot Password</button>
+          <button onClick={handleForgotPassword} disabled={forgotPasswordClicked}>Forgot Password</button>
         </>
       )}
       {step === 2 && (
@@ -130,11 +210,22 @@ const ForgotPassword = () => {
             onChange={(e) => setVerificationCode(e.target.value)}
             required
           />
-          <button onClick={handleVerifyCode}>Verify Code</button>
+          <button onClick={handleVerifyCode} disabled={verifyCodeClicked}>
+            Verify Code
+          </button>
         </>
       )}
       {step === 3 && (
         <>
+          {/* <input
+            type="email"
+            placeholder="Email Address"
+            value={formData.email}
+            onChange={handleChange}
+            disabled
+
+          /> */}
+
           <input
             type="password"
             placeholder="Enter new password"
@@ -149,7 +240,7 @@ const ForgotPassword = () => {
             onChange={(e) => setConfirmPassword(e.target.value)}
             required
           />
-          <button onClick={handlePasswordReset}>Reset Password</button>
+          <button onClick={handlePasswordReset} disabled={resetPasswordClicked}>Reset Password</button>
         </>
       )}
     </div>
