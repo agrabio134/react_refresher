d7100647a93e217dbf26886b2d011b3253262c4f
JPpogi123 <202011111@gordoncollege.edu.ph>
2024-06-03 09:48:35 +0800

    update

--- src/components/pages/home/Service.jsx
+++ src/components/pages/home/Service.jsx
@@ -29,8 +29,8 @@ const ServicePage = () => {
                 <div class="icon">
                   <i className="fa-solid fa-house-chimney-medical"></i>
                 </div>
-                  <h5>Open to All</h5>
-                  <p>General to any kind of pets thatcustomers might have</p>
+                  <h5>Home Service</h5>
+                  <p>Vatan Animal Hospital offers convenient home service care for your furry friends, bringing top-notch veterinary attention directly to your doorstep.</p>
               </div>
 
               <div className="service_content">
@@ -46,7 +46,7 @@ const ServicePage = () => {
                   <i className="fa-solid fa-kit-medical"></i>
                 </div>
                   <h5>Emergency Care</h5>
-                  <p>Services tailored for specific needs like senior pet care, special diets, or medical conditions.</p>
+                  <p>Access to emergency services and immediate attention in critical situations.</p>
               </div>
 
               <div className="service_content">
@@ -54,7 +54,7 @@ const ServicePage = () => {
                   <i className="fa-solid fa-truck-medical"></i>
                 </div>
                   <h5>Specialized Care</h5>
-                  <p>Access to emergency services and immediate attention in critical situations.</p>
+                  <p>Services tailored for specific needs like senior pet care, special diets, or medical conditions.</p>
               </div>
               
             </div>
