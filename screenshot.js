9bdf525e882d96c85564897173c9cd1f8c0e3a3e
John Harvey Agrabio <agrabioharvey@gmail.com>
2024-11-12 12:19:44 +0800

    Update screenshot.js

--- screenshot.js
+++ screenshot.js
@@ -1,8 +1,23 @@
-import puppeteer from 'puppeteer';
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const path = require('path');
 
-(async () => {
+async function captureScreenshot(filePath) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://github.com/agrabio134/react_refresher');
+  const code = fs.readFileSync(filePath, 'utf-8');
+  const formattedCode = `<pre><code>${code}</code></pre>`;
+  
+  await page.setContent(`<html><body>${formattedCode}</body></html>`);
+  await page.screenshot({ path: `${path.basename(filePath)}.png` });
   await browser.close();
-})();
+}
+
+async function main() {
+  const filePaths = ['./src/index.js', './src/utils.js']; // Add paths to the files you want to capture
+  for (const filePath of filePaths) {
+    await captureScreenshot(filePath);
+  }
+}
+
+main();
