c5ba705e1a59f69d3316783b76fbd4c78bf1cf5a
John Harvey Agrabio <agrabioharvey@gmail.com>
2024-11-12 11:35:37 +0800

    Update screenshot.js

--- screenshot.js
+++ screenshot.js
@@ -1,9 +1,8 @@
-const puppeteer = require('puppeteer');
+import puppeteer from 'puppeteer';
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://github.com/agrabio134/react_refresher');
-  await page.screenshot({ path: './screenshot.png' });  // Save screenshot
   await browser.close();
 })();
